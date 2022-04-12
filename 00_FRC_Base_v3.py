108f65c93d267f774be2c50bb025672c1e021622
Jack-Sh <78345769+Jack-Sh@users.noreply.github.com>
2022-04-13 09:52:13 +1200

    FRC COSTS DONE

--- 00_FRC_Base_v3.py
+++ 00_FRC_Base_v3.py
@@ -159,3 +159,7 @@ expense_print("Variable", variable_frame, variable_sub)
 
 if have_fixed == "yes":
     expense_print("Fixed", fixed_frame[['Cost']], fixed_sub)
+
+total_expenses = fixed_sub + variable_sub
+print()
+print("Total Expenses: ${:.2f}".format(total_expenses))
